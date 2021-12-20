3122feebe59fdbe06fccc14d6d3e492f867d5496
unknown <LPT-1944@LPT1944.newgen.co.in>
2021-12-20 14:25:58 +0530

    pagination added

--- src/Components/pages/JobsPosted.js
+++ src/Components/pages/JobsPosted.js
@@ -1,11 +1,39 @@
-import React, { useState, useEffect, useCallback } from "react";
+import * as React from "react";
+import { useState, useEffect, useCallback } from "react";
 import { getjobsApi } from "../Apis/RecruiterApi";
 //import Button from '@mui/material/Button';
 import "../css/style.css";
 function JobsPosted(props) {
   const [jobs, setJobs] = useState([]);
+  const [count, setCount] = useState(0);
   function getApplicants(e) {
     alert("No Applicants found for this job.");
+  }
+  function getPageData(pn) {
+    console.log(pn);
+    let page_n = pn.toString();
+    getjobsApi(page_n, props.token.access_token, (result) => {
+      if (result.data.data.length > 0) {
+        setJobs(result.data.data);
+        setCount(result.data.metadata.count);
+      } else setJobs([]);
+    });
+  }
+  function Pagination(props) {
+    let pages = parseInt(count / 20) + 1;
+    const arr_pages = [...Array(pages).keys()];
+
+    const p_list = arr_pages.map((data, index) => (
+      <a
+        className="page_number"
+        key={index}
+        href="javasript:void(0)"
+        onClick={(e) => getPageData(data + 1)}
+      >
+        {data + 1}
+      </a>
+    ));
+    return p_list;
   }
   function ListofJobs() {
     const m_list = jobs.map((data, index) => (
@@ -34,10 +62,13 @@ function JobsPosted(props) {
   }
   useEffect(() => {
     getjobsApi("1", props.token.access_token, (result) => {
-      if (result.data.data.length > 0) setJobs(result.data.data);
-      else setJobs([]);
+      if (result.data.data.length > 0) {
+        setJobs(result.data.data);
+        setCount(result.data.metadata.count);
+      } else setJobs([]);
     });
   }, []);
+
   return (
     <div>
       {" "}
@@ -54,6 +85,14 @@ function JobsPosted(props) {
       <div className="cardgrid">
         {jobs.length > 0 ? <ListofJobs /> : <span></span>}
       </div>
+      {jobs.length > 0 ? (
+        <div className="pagination">
+          {" "}
+          <Pagination count={count} />
+        </div>
+      ) : (
+        <span></span>
+      )}
     </div>
   );
 }
